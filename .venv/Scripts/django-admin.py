43a546a3bb73aaef927fef80102839595de8ef60
Stuart Yee <ki6fzb@gmail.com>
2021-12-24 19:04:17 -0800

    working basic response

---
+++ .venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\ki6fz\Documents\Code Stuff\DjangoPractice\DjangoForBeginners\.venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
--- pages/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-
-def homePageView(request):
-    return HttpResponse("Hello, world!")
-
-# Create your views here.
